5e92909b7b16b3f1cc9502936afa10e4b9f94288
HoneyHeart3d <violetmorise@hotmail.com>
2026-03-24 15:17:40 -0400

    Healing
    Added Healcube and changed damage mechanics

--- Assets/Scripts/Contact Damage.cs
+++ Assets/Scripts/Contact Damage.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class ContactDamage : MonoBehaviour
 {
-    public int damage;
-    void OnTriggerEnter2D(Collider2D other)
+    public float damage;
+    void OnTriggerStay2D(Collider2D other)
     {
         Debug.Log("enter");
         if (other.CompareTag("Player"))
--- Assets/Scripts/HP Manager.cs
+++ Assets/Scripts/HP Manager.cs
@@ -2,17 +2,18 @@ using UnityEngine;
 
 public class HPManager : MonoBehaviour
 {
-    public int hp;
-    public int hpmax;
+    public float hp;
+    public float hpmax;
     public bool isenemy;
     public GameObject deathmenu;
     void Start()
     {
         hp = hpmax;
     }
-    public void Damage(int dmg)
+    public void Damage(float dmg)
     {
-        hp = hp - dmg;
+        hp = Mathf.Clamp(hp - dmg, 0, hpmax);
+
     }
     void Update()
     {
